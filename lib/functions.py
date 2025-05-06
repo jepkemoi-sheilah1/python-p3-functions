0990bcf6c3d8f2f1f02364ab01171687231a77df
Jepkemoi Sheilah <jepkemoishyllah@gmail.com>
2025-05-06 15:50:26 +0300

    created python function to complete and make pytest pass

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 
-def greet_programmer():
-    pass
+def greet_programmer(): 
+  print("Hello, programmer!")
 
-def greet(name):
-    pass
+def greet(name="programmer"):
+ print(f"Hello, {name}!")
+greet_programmer()
 
 def greet_with_default(name="programmer"):
-    pass
+ print(f"Hello, {name}!")
 
 def add(num1, num2):
-    pass
+      return(num1 + num2)
+
 
 def halve(number):
-    pass
+ return(number / 2)
